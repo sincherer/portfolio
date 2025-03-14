c349c15aae75143ec7da0fd262c68571af9dd048
sincherer <ersincher@gmail.com>
2025-03-14 15:56:27 +0800

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,21 @@ import TogaGoCase from "./pages/TogaGoCase";
 import GoKudosCaseStudy from "./pages/GoKudos";
 
 const App = () => {
-  console.log("App rendering...");
-
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Navigate to="/portfolio" replace />} />
-        <Route path="home" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="articles" element={<Blog />} />
-        <Route path="articles/:id" element={<BlogPost />} />
-        <Route path="projects" element={<Project />} />
-        <Route path="projects/pulsifi-case-1" element={<PulsifiCase01 />} />
-        <Route path="projects/pulsifi-case-2" element={<PulsifiCase02 />} />
-        <Route path="projects/pulsifi-case-3" element={<PulsifiCase03 />} />
-        <Route path="projects/knowledge-base-case" element={<KnowledgeBaseCase />} />
-        <Route path="projects/togago-case" element={<TogaGoCase />} />
-        <Route path="projects/gokudos-case" element={<GoKudosCaseStudy />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/articles" element={<Blog />} />
+        <Route path="/articles/:id" element={<BlogPost />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/projects/pulsifi-case-1" element={<PulsifiCase01 />} />
+        <Route path="/projects/pulsifi-case-2" element={<PulsifiCase02 />} />
+        <Route path="/projects/pulsifi-case-3" element={<PulsifiCase03 />} />
+        <Route path="/projects/knowledge-base-case" element={<KnowledgeBaseCase />} />
+        <Route path="/projects/togago-case" element={<TogaGoCase />} />
+        <Route path="/projects/gokudos-case" element={<GoKudosCaseStudy />} />
       </Routes>
     </Layout>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-const Navbar = () => {
+const Header = () => {
   const location = useLocation();
+  const currentPath = location.pathname.replace('/portfolio', '');
 
   return (
     <nav className="w-full flex justify-center py-4">
       <div className="bg-white shadow-md rounded-full px-6 py-2 flex space-x-6">
-        {["Home","About", "Projects", "Articles", ].map((item) => (
+        {["Home","About", "Projects", "Articles"].map((item) => (
           <Link
             key={item}
             to={`/${item.toLowerCase()}`}
             className={`relative px-3 py-1 text-gray-600  transition ${
-              location.pathname === `/${item.toLowerCase()}`
+              currentPath === `/${item.toLowerCase()}`
                 ? "font-semibold text-black"
                 : "hover:text-white, hover:bg-gray-200 hover:font-semibold, hover:scale-110, hover:duration-300 rounded-full"
             }`}
           >
             {item}
-            {location.pathname === `/${item.toLowerCase()}` && (
+            {currentPath === `/${item.toLowerCase()}` && (
               <span className="absolute bottom-0 left-0 right-0 h-[2px] bg-black rounded-full"></span> 
             )}
           </Link>
@@ -28,4 +29,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Header;
